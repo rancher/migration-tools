accf43cbb8595b01ab021e454641324cd774f056
Prachi Damle <prachi@rancher.com>
2018-09-21 15:11:30 -0700

    Code that parses the docker-compose.yml and rancher-compose.yml and outputs a list of constructs that need special handling in K8s. Also this tool can run Kompose tool to generate K8s YAML specs

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli"
+)
+
+var VERSION = "v0.0.0-dev"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Rancher 1.6 to Rancher 2.0 migration-helper"
+	app.Version = VERSION
+	app.Usage = "Please check the options using --help flag"
+	app.Action = func(c *cli.Context) error {
+		StartTool(c)
+		return nil
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "docker-file",
+			Usage: "An absolute path to an alternate Docker compose file (default: docker-compose.yml)",
+			Value: "docker-compose.yml",
+		},
+		cli.StringFlag{
+			Name:  "rancher-file",
+			Usage: "An absolute path to an alternate Rancher compose file (default: rancher-compose.yml)",
+			Value: "rancher-compose.yml",
+		},
+		cli.StringFlag{
+			Name:  "kompose-tool-path",
+			Usage: "An absolute path to the Kompose tool",
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func StartTool(c *cli.Context) {
+	if c.GlobalBool("debug") {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	textFormatter := &logrus.TextFormatter{
+		FullTimestamp: true,
+	}
+	logrus.SetFormatter(textFormatter)
+
+	logrus.Info("---Starting migration helper tool---")
+
+	t := &ToolArgs{}
+
+	validateFlags(c, t)
+
+	err := setupConstructsMap(t)
+	if err != nil {
+		logrus.Fatalf("Failed to parse the constructs input file, error: %v", err)
+	}
+
+	err = runParser(t)
+	if err != nil {
+		logrus.Fatalf("Failed parsing the compose files, error: %v", err)
+	}
+
+	if t.komposeTool != "" {
+		err := runKompose(t)
+		if err != nil {
+			logrus.Errorf("Failed running the Kompose tool to generate Kuberntes YAML files, error: %v", err)
+		}
+	}
+}
+
+func beforeApp(c *cli.Context) error {
+	if c.GlobalBool("verbose") {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	return nil
+}
+
+func validateFlags(c *cli.Context, t *ToolArgs) {
+	t.dockerComposeFile = c.GlobalString("docker-file")
+	validateDefaultCompose(t)
+
+	t.rancherComposeFile = c.GlobalString("rancher-file")
+	if !isRancherComposePresent(t) {
+		t.rancherComposeFile = ""
+		logrus.Infof("rancher-compose.yml not found, will parse docker-compose.yml only.")
+	}
+
+	if c.IsSet("kompose-tool-path") {
+		t.komposeTool = c.GlobalString("kompose-tool-path")
+	}
+}
+
+func validateDefaultCompose(t *ToolArgs) {
+	_, err := os.Stat(t.dockerComposeFile)
+	if err != nil {
+		logrus.Fatalf("'%s' not found: %v", DefaultDockerComposeFile, err)
+	}
+}
+
+func isRancherComposePresent(t *ToolArgs) bool {
+	_, err := os.Stat(t.rancherComposeFile)
+	if err != nil {
+		return false
+	}
+	return true
+}
